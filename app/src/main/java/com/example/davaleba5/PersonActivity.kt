24c281ff23896eae4968067d2e5e4d12c3978fe4
beka <beka.tchankvetadze.1@btu.edu.ge>
2020-12-19 01:22:25 +0400

    first commit

---
+++ app/src/main/java/com/example/davaleba5/PersonActivity.kt
@@ -0,0 +1,38 @@
+package com.example.davaleba5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
+
+class PersonActivity : AppCompatActivity() {
+
+    private lateinit var userInfoTextView: TextView
+    private lateinit var passwordChangeButton: Button
+    private lateinit var logoutButton: Button
+
+    private lateinit var mAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_person)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        userInfoTextView = findViewById(R.id.userInfoTextView)
+        passwordChangeButton = findViewById(R.id.gotoChangePasswordButton)
+        logoutButton = findViewById(R.id.logoutButton)
+
+        userInfoTextView.text = mAuth.currentUser?.uid
+
+        passwordChangeButton.setOnClickListener {
+
+        }
+
+        logoutButton.setOnClickListener {
+
+        }
+
+    }
+}
